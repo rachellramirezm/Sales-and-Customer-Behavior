770f59da69ed9f4ba8e0db0d3b3d743701d72729
RachelRamírez <rachell.ramirezm@gmail.com>
2026-01-27 16:32:46 -0600

    Add files via upload

--- sql/schema/create_database_structure.sql
+++ sql/schema/create_database_structure.sql
@@ -1,1 +1,37 @@
 
+/*
+First, create the database with this script "CREATE DATABASE sales_and_customer_behavior", then connect to the previously created database and run this script.
+*/
+CREATE SCHEMA IF NOT EXISTS staging; --Create schema to work in layers
+
+--Table: staging.product_information
+CREATE TABLE staging.product_information (
+    product_id TEXT,
+    product_name TEXT,
+    category_name TEXT,
+    launch_date TEXT,
+    base_price TEXT,
+    supplier_code TEXT
+);
+
+CREATE TABLE staging.customer_information (
+    customer_id TEXT,
+    email TEXT,
+    signup_date TEXT,
+    gender TEXT,
+    region TEXT,
+    loyalty_tier TEXT
+);
+
+CREATE TABLE staging.sales_information (
+    order_id TEXT,
+    customer_id TEXT,
+    product_id TEXT,
+    quantity TEXT,
+    unit_price TEXT,
+    order_date TEXT,
+    delivery_status TEXT,
+    payment_method TEXT,
+    region TEXT,
+    discount_applied TEXT
+);
